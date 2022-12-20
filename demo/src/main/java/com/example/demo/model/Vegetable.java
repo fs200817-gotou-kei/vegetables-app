dd6f3a43d924ff01f6fda32dd5b964ad94f4999c
LAPTOP-N8E0I7QK\gotou <gotoukei57@gmail.com>
2022-12-20 22:42:58 +0900

    vegetableのモデルクラス作成

---
+++ demo/src/main/java/com/example/demo/model/Vegetable.java
@@ -0,0 +1,41 @@
+package com.example.demo.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "VEGETABLE")
+public class Vegetable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "PRICE")
+    private int price;
+
+    @Column(name = "CREATED_AT")
+    private LocalDateTime createdAt;
+
+    @Column(name = "UPDATED_AT")
+    private LocalDateTime updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "COLOR_ID", insertable = false, updatable = false)
+    private Color color;
+
+}
